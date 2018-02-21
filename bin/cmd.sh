398b0095d4b824120fe87883477c7c463935f1e3
Cloud User <centos@ip-172-31-45-25.us-west-2.compute.internal>
2018-02-21 10:13:31 +0000

    modify cmd.sh to control all data download & iterate ego-net's 10 datasets

--- bin/cmd.sh
+++ bin/cmd.sh
@@ -1,20 +1,36 @@
 #!/bin/bash
-cwd=`pwd`
-DATADIR="../data/ego-net/"
+source utils.sh
 REPORTDIR="../report/"
 MAINDIR="../SNE_lab/"
-LEARNING_RATE="0.01"
 
-cd ${MAINDIR}
-#for tag in "3980" "414" "686" "698"; do
-#for tag in "0" "107" "1684"; do
-for tag in "3437" "348" "1912"; do
-  for featNum in 100 200 300 400; do
-    if [ "${featNum}" -ne "400" ]; then
-      NEG_SAMPLE_NUM=1 ITEM_FIELDS_NUM=${featNum} MAX_TRAIN_NUM=5000 LEARNING_RATE=${LEARNING_RATE} MOMENTUM=1.0 LAMBDA=0.001 nohup python -u sne_lab.py 0 10 ego-net rating_file="../data/ego-net/${tag}.edges.u2u" usr2labels_file="../data/ego-net/${tag}.circles.u2f.filtered" sub=${tag} > "${REPORTDIR}${featNum}Fego-net${tag}_out" &
-    else
-      NEG_SAMPLE_NUM=1 ITEM_FIELDS_NUM=${featNum} MAX_TRAIN_NUM=5000 LEARNING_RATE=${LEARNING_RATE} MOMENTUM=1.0 LAMBDA=0.001 nohup python -u sne_lab.py 0 10 ego-net rating_file="../data/ego-net/${tag}.edges.u2u" usr2labels_file="../data/ego-net/${tag}.circles.u2f.filtered" sub=${tag} > "${REPORTDIR}${featNum}Fego-net${tag}_out"
-    fi
-  done
-done
-cd ${cwd}
+## Assert the execution place
+assertCurDir $0
+
+if [ "$1" = "ego" ]; then
+  echo "[info] Will iterate over ego-net's 10 dataSets"
+  DATADIR="../data/ego-net/"
+  LEARNING_RATE="0.005"
+  FoldNum="5"
+  MAX_TRAIN_NUM="1000"
+  cd ${MAINDIR} && {
+    for tag in 348 1912; do
+      for featNum in 100 200 300 400; do
+        # Would 'roughly' run four processes (by featNum)
+        if [ "${featNum}" -ne "400" ]; then
+          NEG_SAMPLE_NUM=1 ITEM_FIELDS_NUM=${featNum} MAX_TRAIN_NUM=${MAX_TRAIN_NUM} LEARNING_RATE=${LEARNING_RATE} MOMENTUM=1.0 LAMBDA=0.001 nohup python -u sne_lab.py 0 ${FoldNum} ego-net rating_file="../data/ego-net/${tag}.edges.u2u" usr2labels_file="../data/ego-net/${tag}.circles.u2f.filtered" sub=${tag} > "${REPORTDIR}${featNum}Fego-net${tag}_out" &
+        else
+          NEG_SAMPLE_NUM=1 ITEM_FIELDS_NUM=${featNum} MAX_TRAIN_NUM=${MAX_TRAIN_NUM} LEARNING_RATE=${LEARNING_RATE} MOMENTUM=1.0 LAMBDA=0.001 nohup python -u sne_lab.py 0 ${FoldNum} ego-net rating_file="../data/ego-net/${tag}.edges.u2u" usr2labels_file="../data/ego-net/${tag}.circles.u2f.filtered" sub=${tag} > "${REPORTDIR}${featNum}Fego-net${tag}_out"
+        fi
+      done
+    done
+    cd -;
+  }
+elif [ "$1" = "data" ]; then
+  echo "[info] Render all dataset"
+  bash ml-100k.sh
+  bash ml-1m.sh
+  bash ego-net.sh
+  bash youtube.sh
+else
+  echo "[Err] Usage: $0 data|ego"
+fi
