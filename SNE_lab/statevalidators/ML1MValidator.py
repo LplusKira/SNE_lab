3f43afc71955384bddd09c7123d26e714d2fe3b6
Cloud User <centos@ip-172-31-34-235.us-west-2.compute.internal>
2018-02-21 08:29:44 +0000

    modify ML1MValidator.py's bug; rm useless old data

--- SNE_lab/statevalidators/ML1MValidator.py
+++ SNE_lab/statevalidators/ML1MValidator.py
@@ -4,7 +4,7 @@ class ML1MValidator(BaseValidator):
         super(ML1MValidator, self).__init__(dataset, datasetSub, curFold, totalFolds, usr2itemsIndxTrain, usr2itemsIndxValid, MAX_TRAIN_NUM, ITEM_FIELDS_NUM, silence=False, predictTrain=False, write2File=True)
 
         # Customized params
-        setRuns(RevealRun=500, CalLossRun=500, cb=setCalLossRun)
+        self.setRuns(RevealRun=200, CalLossRun=200, cb=self.setCalLossRun)
         self.NegSampleFreq = 5
 
     def setRuns(self, RevealRun, CalLossRun, cb):
